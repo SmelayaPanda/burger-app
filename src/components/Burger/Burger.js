8a13844a27de2305dca11c5965c9f5f87857a622
panda <SmelayaPanda@mail.ru>
2018-08-08 15:04:32 +0700

    flat array

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,21 @@ import classes from './Burger.css'
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = props => {
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
       return [...Array(props.ingredients[igKey])] // create an empty array with length equal to ingredients counts
         .map((_, i) => {
           return <BurgerIngredient key={igKey + i} type={igKey}/>
         })
     })
+    .reduce((prev, cur) => { // flat array
+      return prev.concat(cur)
+    }, []) // empty array - initial value
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>
+  }
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type={'bread-top'}/>
