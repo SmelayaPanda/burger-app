db01a736e0a64dfd910cdbceb7ea53aca883afc0
panda <SmelayaPanda@mail.ru>
2018-08-12 19:08:08 +0700

    orders+

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,13 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 import classes from './Order.css'
 
 const order = (props) => {
+  const ingredients = []
+  for (let i in props.ingredients) {
+    ingredients.push({name: i, amount: props.ingredients[i]})
+  }
+
+  let ingredientOutput = ingredients.map(el => {
+    return (
+      <span
+        style={{
+          textTransform: 'capitalize',
+          display: 'inline-block',
+          margin: '0 8px',
+          border: '1px solid #eee',
+          padding: '5px'
+        }}
+        key={el.name}>{el.name} <strong>({el.amount})</strong>
+      </span>
+    )
+  })
   return (
     <div className={classes.Order}>
-      <p>Ingredients: Salad (1)</p>
-      <p>Price: <strong>USD: 5.45</strong></p>
+      <p>Ingredients: {ingredientOutput}</p>
+      <p>Price: <strong>USD: {props.price}</strong></p>
     </div>
   );
 };
+
+order.propTypes = {
+  price: PropTypes.number.isRequired,
+  ingredients: PropTypes.object.isRequired
+}
 
 export default order;
